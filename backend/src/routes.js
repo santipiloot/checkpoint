2f2a45f0604dfc94723cca6b492e926214b9267a
Santino Exequiel Pilot <santinoexequielpilot0@gmail.com>
2026-03-24 12:38:20 -0300

    feat: implementar modulo de categorias y añadir verificacion de autenticacion a rutas privadas

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,15 @@
 import { Router } from 'express';
 import authRoutes from './modulos/auth/auth.routes.js';
+import categoriaRoutes from './modulos/categorias/categoria.routes.js';
+import { verificarAutenticacion } from './middlewares/auth.middleware.js';
 
 const router = Router();
 
 // Rutas publicas
-
 router.use('/auth', authRoutes);
 
-// Rutas privadas 
+// Rutas privadas
+router.use(verificarAutenticacion);
+router.use('/categorias', categoriaRoutes);
 
 export default router;

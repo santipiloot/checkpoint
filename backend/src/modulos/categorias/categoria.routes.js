bb42f00b91f878abb4bcde9ee59cd2c6f2d9a74c
Santino Exequiel Pilot <santinoexequielpilot0@gmail.com>
2026-03-24 14:30:02 -0300

    refactor: unificar las validaciones de crear y actualizar una categoria en una sola

--- backend/src/modulos/categorias/categoria.routes.js
+++ backend/src/modulos/categorias/categoria.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import * as categoriaController from "./categoria.controller.js";
 import { verificarRoles } from "../../middlewares/rol.middleware.js";
-import { validarId, validarCrearCategoria, validarActualizarCategoria } from "./categoria.validator.js";
+import { validarId, validarCategoria } from "./categoria.validator.js";
 
 const router = Router();
 
@@ -10,9 +10,9 @@ router.get("/", categoriaController.obtenerCategorias);
 router.get("/:id", validarId, categoriaController.obtenerCategoriaPorId);
 
 // Solo admin
-router.post("/", [verificarRoles(['admin']), validarCrearCategoria], categoriaController.crearCategoria);
+router.post("/", [verificarRoles(['admin']), validarCategoria], categoriaController.crearCategoria);
 
-router.put("/:id", [verificarRoles(['admin']), validarId, validarActualizarCategoria], categoriaController.actualizarCategoria);
+router.put("/:id", [verificarRoles(['admin']), validarId, validarCategoria], categoriaController.actualizarCategoria);
 
 router.delete("/:id", [verificarRoles(['admin']), validarId], categoriaController.eliminarCategoria);
 

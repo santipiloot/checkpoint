da7e84d4bf237d1c16af7d10410abd125209c045
Nazareno Exequiel Mansilla <mansillanaza@gmail.com>
2026-03-28 04:03:44 -0300

    Se termina service de movimientos

---
+++ backend/src/modulos/movimientos/movimientos.service.js
@@ -0,0 +1,31 @@
+import * as movimientosRepository from './movimientos.repository.js';
+import * as productosRepository from '../productos/productos.repository.js';
+
+
+export const obtenerMovimientos = async (filtros) => {
+    return await movimientosRepository.obtenerMovimientos(filtros);
+};
+
+
+export const crearMovimiento = async (datosMovimiento) => {
+   
+    const producto = await productosRepository.productoPorId(datosMovimiento.producto_id);
+    
+    if (!producto) {
+        const error = new Error("El producto no existe");
+        error.status = 404;
+        throw error;
+    }
+
+    if (datosMovimiento.tipo === 'salida') {
+        if (producto.stock < datosMovimiento.cantidad) {
+            const error = new Error(`Stock insuficiente`);
+            error.status = 400;
+            throw error;
+        }
+    }
+
+    const resultado = await movimientosRepository.crearMovimiento(datosMovimiento);
+
+    return resultado;
+};

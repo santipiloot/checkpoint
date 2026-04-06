f51b0959bedc10610c76a601ac7a847f293d2785
Nazareno Exequiel Mansilla <mansillanaza@gmail.com>
2026-04-06 18:31:22 -0300

    Se modifica el nombre de la carpeta modulos a modules y se elimina el optional en el body de usuarios

--- backend/src/modules/usuarios/usuarios.validator.js
+++ backend/src/modules/usuarios/usuarios.validator.js
@@ -9,15 +9,12 @@ export const validarId = [
 
 export const validarUsuario = [
     body('nombre')
-        .optional()
         .isString().withMessage('El nombre debe ser texto')
         .notEmpty().withMessage('El nombre es obligatorio')
         .isLength({ max: 100, min: 3 }).withMessage('El nombre debe tener entre 3 y 100 caracteres'),
     body('email')
-        .optional()
         .isEmail().withMessage('Debe ser un email válido'),
     body('rol')
-        .optional()
         .isIn(['admin', 'empleado']).withMessage('El rol debe ser admin o empleado'),
     verificarValidaciones
 ];

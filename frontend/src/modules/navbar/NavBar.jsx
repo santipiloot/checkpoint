8478f81fb7e267f0056c8994ea9de9ec2ca7d7a9
Nazareno Exequiel Mansilla <mansillanaza@gmail.com>
2026-04-22 11:24:52 -0300

    Se arregla NavBar

--- frontend/src/modules/navbar/NavBar.jsx
+++ frontend/src/modules/navbar/NavBar.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { Link, Outlet } from "react-router";
+import { useAuth } from "../../context/AuthContext.jsx";
 
 function NavBar() {
-  return <div>NavBar</div>;
+  const { isAuthenticated, logout, rol } = useAuth();
+
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+
+          <li>
+            <Link to="/proveedores">Proveedores</Link>
+          </li>
+          {isAuthenticated ? (
+            <li>
+              <button onClick={logout}>Cerrar Sesión</button>
+            </li>
+          ) : (
+            <li>
+              <Link to="/login">Iniciar Sesion</Link>
+            </li>
+          )}
+        </ul>
+      </nav>
+      <Outlet />
+    </div>
+  );
 }
 
 export default NavBar;

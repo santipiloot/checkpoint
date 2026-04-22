3ac8beba688dd17f1be668f99880917edff59d12
Nazareno Exequiel Mansilla <mansillanaza@gmail.com>
2026-04-21 21:54:28 -0300

    Login basico creado

--- frontend/src/modules/login/Login.jsx
+++ frontend/src/modules/login/Login.jsx
@@ -1,5 +1,39 @@
-import React from "react";
+import { useState } from "react";
+import { useNavigate, Link } from "react-router";
+import { useAuth } from "../../context/AuthContext";
 
 export default function Login() {
-  return <div>Login</div>;
+  const navigate = useNavigate();
+  const { error, login } = useAuth();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const res = await login(email, password);
+
+    if (res.success) {
+      navigate("/");
+    }
+  };
+
+  return (
+    <>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </>
+  );
 }
